01dc2401c84efbd8cc08c3d4b0ded26197b55768
Gaston Roman Lopez De Lacalle <gasroman@car5s3.42barcelona.com>
2024-10-24 15:44:36 +0200

    push before merge

--- src/executer/executer.c
+++ src/executer/executer.c
@@ -1,24 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   core.h                                             :+:      :+:    :+:   */
+/*   executer.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gasroman <gasroman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/23 14:34:33 by tatahere          #+#    #+#             */
-/*   Updated: 2024/10/24 15:42:15 by gasroman         ###   ########.fr       */
+/*   Created: 2024/10/24 15:01:31 by gasroman          #+#    #+#             */
+/*   Updated: 2024/10/24 15:16:14 by gasroman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CORE_H
-# define CORE_H
 
-//	this function will give the line to the core to interpret and execute
-int		run_comand(char *str, void *env_struct);
-
-//	comands to work with the env module
-char	**read_env(void *env_struct);
-void	push_env(void *env_env);
-void	pop_env(void *env_env);
-
-#endif
+int	executer(t_strutct prompt)
+{
+}
